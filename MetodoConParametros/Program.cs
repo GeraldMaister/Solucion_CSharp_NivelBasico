4b4f48cece1dacb3f7f0a46837112bfd0107184b
GeraldMaister <geraldmaister@hotmail.com>
2023-03-11 13:19:55 -0400

    Metodos y Excepciones

---
+++ MetodoConParametros/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoConParametros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Metodos con parametros
+             * el parametro es lo que va en el parentesis que esta esperando un dato para poder proceden con el metodo.
+             * 
+             
+             */
+
+            string OperacionMatematica;
+            int nOperacion;
+
+            Console.Write("Quieres realizar la tabla de Sumar o Multiplicar? " );
+            OperacionMatematica = Console.ReadLine();
+
+            Console.Write("Que numero de tabla desea que se muestre? ");
+            nOperacion = int.Parse(Console.ReadLine());
+
+            if(OperacionMatematica == "Sumar" ) 
+            {
+                Sumar(nOperacion);
+            }
+            else if (OperacionMatematica == "Multiplicar")
+            {
+                Multiplicar(nOperacion);
+            }
+             else
+            {
+                Console.WriteLine("Operacion matematica no valida");
+            }
+
+
+            Console.ReadKey();
+        }
+        // metodo de la tabla de Sumar
+          static void Sumar(int Numero) 
+          { 
+            for(int a = 1; a<=12; a++)
+            {
+                Console.WriteLine("{0} + {1} = {2}", Numero, a, (Numero+a));
+            }
+
+        
+          }
+
+        //Método de la tabla de multiplicar
+        static void Multiplicar(int Numero)
+        {
+            for (int a = 1; a <= 12; a++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", Numero, a, (Numero * a));
+            }
+
+
+        }
+    }
+}
---
+++ MetodoConRetorno/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoConRetorno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Metodo con Retorno de resultado
+             * para devolver un resultado el tipo de static y el tipo de dato, 
+             * ej:  static double CalcularTotal(double P1,int P2) porque va retornar un double.
+             */
+            string nombre;
+            double pagoxdia;
+            int dias;
+            
+
+            Console.Write("Ingresa tu Nombre: ");
+            nombre= Console.ReadLine();
+
+            Console.Write("Cuanto te pagan por dia trabajado? ");
+            pagoxdia = double.Parse(Console.ReadLine());
+
+            Console.Write("Cuantos dias trabajaste? ");
+            dias = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El dinero obtenido por los dias de trabajo es: {0}", CalcularTotal(pagoxdia, dias));
+            Console.ReadKey();
+        }
+        // metodo para obtener el total de dinero ganado por trabajo
+
+
+        static double CalcularTotal(double P1,int P2)
+        {
+            
+
+            double resultado;
+
+            resultado = (P1 * P2);
+
+            return resultado;
+        }
+    }
+}
